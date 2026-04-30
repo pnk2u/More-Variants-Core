6bdac90215aff525f4a33f415f1955504d3763bc
pnku <pnku@pnku.de>
2026-04-30 02:57:50 +0200

    Remove WoodTypes

--- src/main/java/de/pnku/more_variants_core/util/WoodTypes.java
+++
@@ -1,9 +0,0 @@
-package de.pnku.more_variants_core.util;
-
-import net.minecraft.world.level.material.MapColor;
-
-import static com.blackgear.vanillabackport.common.registries.ModBlocks.PALE_OAK_PLANKS;
-
-public class WoodTypes {
-        public static final WoodType PALE_OAK = new WoodType("pale_oak", MapColor.QUARTZ, PALE_OAK_PLANKS.get());
-}
