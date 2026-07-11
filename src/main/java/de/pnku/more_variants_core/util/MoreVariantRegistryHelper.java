797c9385164219b7bdf581ab876e5e6a43047869
pnku <pnku@pnku.de>
2026-07-11 21:17:27 +0200

    Stylistic changes in MoreVariantRegistryHelper (`1.0.2-d:c4`)

--- src/main/java/de/pnku/more_variants_core/util/MoreVariantRegistryHelper.java
+++ src/main/java/de/pnku/more_variants_core/util/MoreVariantRegistryHelper.java
@@ -12,24 +12,24 @@ public class MoreVariantRegistryHelper {
     public static void whenItemRegistered(ResourceLocation itemId, Consumer<Item> action) {
         if (BuiltInRegistries.ITEM.containsKey(itemId)) {
             action.accept(BuiltInRegistries.ITEM.get(itemId));
-            return;
+        } else {
+            RegistryEntryAddedCallback.event(BuiltInRegistries.ITEM).register((itemIntId, id, item) -> {
+                if (id.equals(itemId)) {
+                    action.accept(item);
+                }
+            });
         }
-        RegistryEntryAddedCallback.event(BuiltInRegistries.ITEM).register((itemIntId, id, item) -> {
-            if (id.equals(itemId)) {
-                action.accept(item);
-            }
-        });
     }
 
     public static void whenBlockRegistered(ResourceLocation blockId, Consumer<Block> action) {
         if (BuiltInRegistries.BLOCK.containsKey(blockId)) {
             action.accept(BuiltInRegistries.BLOCK.get(blockId));
-            return;
+        } else {
+            RegistryEntryAddedCallback.event(BuiltInRegistries.BLOCK).register((blockIntId, id, block) -> {
+                if (id.equals(blockId)) {
+                    action.accept(block);
+                }
+            });
         }
-        RegistryEntryAddedCallback.event(BuiltInRegistries.BLOCK).register((blockIntId, id, block) -> {
-            if (id.equals(blockId)) {
-                action.accept(block);
-            }
-        });
     }
 }
